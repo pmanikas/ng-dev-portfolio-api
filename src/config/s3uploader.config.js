f6572e106e920cea88e127b7f85b7436ed8e1f5d
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-21 22:43:02 +0200

    Format files

--- src/config/s3uploader.config.js
+++ src/config/s3uploader.config.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -10,21 +10,21 @@ const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new aws.S3({
-  region,
-  accessKeyId,
-  secretAccessKey
+    region,
+    accessKeyId,
+    secretAccessKey,
 });
 
 module.exports = uploadToS3 = multer({
-  storage: multerS3({
-    s3,
-    bucket: bucketName,
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.originalname });
-    },
-    key: (req, file, cb) => {
-      let finalFileName = file.originalname;
-      cb(null, finalFileName);
-    }
-  })
+    storage: multerS3({
+        s3,
+        bucket: bucketName,
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.originalname });
+        },
+        key: (req, file, cb) => {
+            let finalFileName = file.originalname;
+            cb(null, finalFileName);
+        },
+    }),
 });
